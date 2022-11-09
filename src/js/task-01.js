4fd79097481bfe5a4b338ca0741676950804e549
katya-savytska <108482673+katya-savytska@users.noreply.github.com>
2022-11-09 15:35:09 +0200

    hw

---
+++ src/js/task-01.js
@@ -0,0 +1,7 @@
+const categoryEl = document.querySelectorAll('li.item');
+const numbOfEl = categoryEl.length;
+console.log('Number of categories:', numbOfEl);
+const categoryTitleEl = document.querySelector('h2');
+const categoryTitle = categoryTitleEl.textContent;
+console.log('Categoty: ', categoryTitle);
+
