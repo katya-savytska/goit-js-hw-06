6b502405b8982c11c0ecd1854856019f6d0d115a
Савицька Катерина <savytskaa.kate@gmail.com>
2022-12-12 14:06:49 +0300

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,7 +1,11 @@
-const categoryEl = document.querySelectorAll('li.item');
-const numbOfEl = categoryEl.length;
+/* eslint-disable linebreak-style */
+const categoryList = document.querySelectorAll('li.item');
+console.log(categoryList);
+const numbOfEl = categoryList.length;
 console.log('Number of categories:', numbOfEl);
+
 const categoryTitleEl = document.querySelector('h2');
 const categoryTitle = categoryTitleEl.textContent;
-console.log('Categoty: ', categoryTitle);
+console.log('Category: ', categoryTitle);
+
 
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,3 +1,5 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-unused-vars */
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +8,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+console.log(ingredients);
+
+const makeList = (ingredient) => {
+  const list = document.querySelector('#ingredients');
+  const ingredientsEl = document.createElement('li');
+  ingredientsEl.classList.add('item');
+  ingredientsEl.textContent = ' ';
+  console.log(ingredientsEl);
+  list.append(ingredientsEl);
+
+  return list;
+};
+const elements = ingredients.map(makeList);
+console.log(elements);
