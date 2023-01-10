d15b7a92f769dc376523671f31f9d3bd7693f70d
Савицька Катерина <savytskaa.kate@gmail.com>
2023-01-10 11:49:01 +0300

    task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable require-jsdoc */
+/* eslint-disable linebreak-style */
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: {email, password},
+  } = event.currentTarget;
+
+  if (email.value.trim === '' ||
+    password.value === '') {
+    return alert('Please fill in all the fields!');
+  }
+  const userDetails = {email:
+    email.value, Password: password.value};
+
+  console.log(userDetails);
+  event.currentTarget.reset();
+}
