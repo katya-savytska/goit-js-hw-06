8fafd41d0db2f4208a9422b920016ecabe63510f
Савицька Катерина <savytskaa.kate@gmail.com>
2023-01-10 12:02:51 +0300

    task 09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,21 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable require-jsdoc */
+/* eslint-disable linebreak-style */
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const bodyEl = document.querySelector('body');
+
+const btnColor = document.querySelector('.change-color');
+console.log(btnColor);
+btnColor.addEventListener('click', changeColor);
+
+const spanColor = document.querySelector('.color');
+console.log(spanColor);
+
+function changeColor() {
+  const color = getRandomHexColor();
+  spanColor.textContent = color;
+  bodyEl.style.backgroundColor = color;
 }
